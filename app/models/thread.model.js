7d44240419149e849adcf1d782689653aea30940
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-03-26 20:03:48 +0700

    hooks, statics, and prevent circular deps

--- app/models/thread.model.js
+++ app/models/thread.model.js
@@ -4,7 +4,6 @@ const { model, Schema, SchemaTypes } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const { models, timestamps } = require("../../config").mongooseConfig;
-const { Board } = require("./board.model");
 
 const threadSchema = new Schema(
   {
@@ -43,14 +42,39 @@ threadSchema.virtual("replycount").get(function () {
 
 threadSchema.pre("save", async function (next) {
   // add thread id to board's threads array when a thread is created
-  await Board.findByIdAndUpdate(
-    this.board_id,
+  await model(models.Board).findByIdAndUpdate(
+    this.board,
     { $push: { threads: this._id } },
     { new: true }
   );
+  // await Board.findByIdAndUpdate(
+  //   this.board,
+  //   { $push: { threads: this._id } },
+  //   { new: true }
+  // );
 
   // hash delete password
   this.delete_password = await bcrypt.hash(this.delete_password, 10);
+
+  next();
+});
+
+threadSchema.pre("remove", async function (next) {
+  // remove thread id from board's threads array when a thread is removed
+  await model(models.Board).findByIdAndUpdate(
+    this.board,
+    { $pull: { threads: this._id } },
+    { new: true }
+  );
+  // await Board.findByIdAndUpdate(
+  //   this.board,
+  //   { $pull: { threads: this._id } },
+  //   { new: true }
+  // );
+
+  // remove replies from thread's replies array when a thread is removed
+  await model(models.Reply).deleteMany({ thread: this._id });
+  // await Reply.deleteMany({ thread: this._id });
 
   next();
 });
@@ -59,7 +83,7 @@ threadSchema.pre("save", async function (next) {
  * Mock a thread document.
  *
  * @param {Object} thread - A thread object with the following properties:
- * - board_id: {SchemaTypes.ObjectId} - The _id field of the board document
+ * - board: {SchemaTypes.ObjectId} - The _id field of the board document
  *  that the thread belongs to.
  * - text: {String} - The text of the thread.
  * - replies: {Array<SchemaTypes.ObjectId>} - The _id fields of the reply
@@ -67,16 +91,18 @@ threadSchema.pre("save", async function (next) {
  * @return {Object} A thread object.
  */
 threadSchema.statics.mock = function ({
-  board_id,
+  board,
   text,
   replies,
   reported,
+  delete_password
 } = {}) {
   return {
-    board_id: board_id || "5e9c9c9c9c9c9c9c9c9c9c9c",
+    board: board || "",
     text: text || "Thread Mock",
     replies: replies || [],
     reported: reported || Math.random() > 0.5,
+    delete_password: delete_password || "password",
   };
 };
 
@@ -88,8 +114,36 @@ threadSchema.statics.mock = function ({
  */
 threadSchema.statics.reportThread = async function (threadId) {
   const thread = await this.findById(threadId);
+  if (!thread) {
+    throw new Error("resource not found");
+  }
   thread.reported = true;
   return await thread.save();
+};
+
+/**
+ * Delete a thread with valid delete_password.
+ *
+ * @param {SchemaTypes.ObjectId} threadId - Thread document _id field.
+ * @param {String} deletePassword - The delete password of the thread.
+ * @return {Promise<Thread>} A promise that resolves to a Thread document.
+ */
+threadSchema.statics.deleteThread = async function (threadId, deletePassword) {
+  const thread = await this.findById(threadId);
+  if (!thread) {
+    throw new Error("resource not found");
+  }
+
+  const isMatch = await bcrypt.compare(deletePassword, thread.delete_password);
+  if (!isMatch) {
+    throw new Error("incorrect password");
+  }
+
+  // delete the referenced replies
+  await model(models.Reply).deleteMany({ thread: threadId });
+  // await Reply.deleteMany({ thread: thread._id });
+
+  return await thread.remove();
 };
 
 /**
